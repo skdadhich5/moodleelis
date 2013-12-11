406db1960cae55c70adf930b9901e011c3ea4af9
sdadhich <skdadhich5@gmail.com>
2013-12-12 00:27:52 +0530

    Elis plugins applied

---
+++ auth/openid/version.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * OpenID module version information
+ *
+ * @author Brent Boghosian <brent.boghosian@remote-learner.net>
+ * @copyright Copyright (c) 2011 Remote-Learner
+ * @author Stuart Metcalfe <info@pdl.uk.com>
+ * @copyright Copyright (c) 2007 Canonical
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package openid
+ */
+
+$plugin->version  = 2013062500;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2013051400;  // Requires this Moodle version
+$plugin->release = '2.5.1.0';    // ELIS Component Version
